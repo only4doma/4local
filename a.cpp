dd69c70e60e785725a864041462aed1e3d94df3e
only4doma <git4doma@gmail.com>
2023-06-30 04:06:43 +0530

    both a.cpp & sqrt.cpp are modefied

--- a.cpp
+++ a.cpp
@@ -1,7 +1,1 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main() {
-cout << "Hello World!" << endl;
-return 0;
-}
+print("Hello World!")
--- sqrt.cpp
+++ sqrt.cpp
@@ -1,40 +1,27 @@
-#include<bits/stdc++.h>
-using namespace std;
+def giveRoot(num):
+    lt = 0
+    rt = num
+    ans = 0
+    
+    while lt <= rt:
+        mid = lt + (rt - lt) // 2
+        if mid * mid == num:
+            return mid
+        if mid * mid < num:
+            ans = mid
+            lt = mid + 1
+        else:
+            rt = mid - 1
+    
+    dec = 0.1
+    
+    for i in range(1, 11):
+        while ans * ans <= num:
+            ans += dec
+        ans -= dec
+        dec /= 10
+    
+    return ans
 
-double giveRoot(int num) {
-	int lt = 0, rt = num;
-	double ans = 0;
-	
-	while(lt <= rt){
-		int mid = lt + (rt - lt) / 2;
-		if((mid * mid) == num){
-			return mid;
-		}
-		if((mid * mid) < num){
-			ans = mid;
-			lt = mid + 1;
-		}
-		else {
-			rt = mid - 1;
-		}
-	}
-	
-	double dec = 0.1;
-	
-	for(int i = 1; i <= 10; ++i){
-		while((ans * ans) <= num){
-			ans += dec;
-		}
-		ans -= dec;
-		dec /= 10;
-	}
-	return ans;
-}
-
-int main() {
-	int n;
-	cin >> n;
-	
-	printf("%0.10lf", giveRoot(n));
-	return 0;
-}
+n = int(input())
+print("{:.10f}".format(giveRoot(n)))
